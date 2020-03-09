0ad96ea6d4885c03971f0d338cf29597c9568076
Ziyue-Zhang <2846728884@qq.com>
2020-03-09 10:18:13 +0800

    “start”

---
+++ Code/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    if(argc>1){
+        if(!(yyin=fopen(argv[1],"r"))){
+            perror(argv[1]);
+            return 1;
+        }
+    }
+    while(yylex()!=0);
+    return 0;
+}
